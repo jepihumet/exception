fddf4170b7dcc6b31a6f79f6f3f5b26123830205
Jepi Humet <jepihumet@gmail.com>
2016-03-28 12:15:00 +0200

    First commit with code files

---
+++ src/Exception/JepiException.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * JepiException.php
+ *
+ * @package     JepiFW
+ * @author      Jepi Humet Alsius <jepihumet@gmail.com>
+ * @link        http://jepihumet.com
+ */
+
+namespace Jepi\Exception;
+
+use Jepi\IO\Response;
+
+class JepiException extends \Exception
+{
+    protected $exceptionType = "Exception";
+
+    public function getProductionMessage()
+    {
+        $texts = Response::$statusTexts;
+        if (array_key_exists($this->getCode(), $texts)){
+            $message = $texts[$this->getCode()];
+        }else{
+            $message = 'Unknown error';
+        }
+        $errorMsg = sprintf('Error $s: $s', $this->getCode(), $message);
+        return $errorMsg;
+    }
+    public function getDevelopmentMessage()
+    {
+        $errorMsg = sprintf('Error on line %s in %s: %s',$this->getLine(),$this->getFile(),$this->getMessage());
+        return $errorMsg;
+    }
+
+    public function getExceptionType(){
+        return $this->exceptionType;
+    }
+}
